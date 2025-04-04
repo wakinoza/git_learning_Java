1d521deaf8fcdebe10eb6f48c04eefe002f11df4
azu-wakinoza <wakinoza@gmail.com>
2025-04-04 12:47:46 +0900

    例外処理の練習問題
    例外は①文法エラー②実行時エラー③論理エラーの３つに分けられる。
    ①文法エラーは、コードの文法の誤りで、コンパイル時にエラーが出る。
    ②実行時エラーは、コードの文法は正しいが、実行中に何らかのエラーが発生したために、プログラムが強制停止したもの。
    ③論理エラーは、文法も正しく、プログラムも最後まで実行されるが、想定外の結果が出るもの。
    ①文法エラーと③論理エラーは開発者のミスなので、入念にテスト行えば、エラー発生を予防できる。
    しかし、②実行時エラーは、プログラム実行中に起きた想定外の事態（例外）によって起こります。
    例外のうち、対処しよう方法がある重要な例外は「チェック例外」と呼ばれます。
    チェック例外が発生する可能性がある命令を使用するときは、必ず例外処理が必要。
    例外処理は、①try-catch分でキャッチ指定処理するか、②メソッド定義時にthrows宣言を加えて呼び出し元に例外を伝播させる処理が必要。
    この２つのどちらかの例外処理がないと、コンパイラエラーとなる。
    そのほかに、例外的状況であると開発者からJVMに知らせたい場合は、throwで例外をJVMに投げることもできる。

---
+++ ExMain .java
@@ -0,0 +1,14 @@
+public class ExMain {
+  public static void main(String[] args) { 
+      try{
+          String s = null;
+          System.out.println(s.length());
+      }catch(NullPointerException e){
+          System.out.println("NullPointerException例外をcatchしました");
+          System.out.println("----スタックトレース（ここから）----");
+          e.printStackTrace();
+          System.out.println("----スタックトレース（ここまで）----");
+      }
+      
+  }
+}
