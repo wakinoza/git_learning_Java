945ae0d1f9df52394fb129cfda11f76fdb7c2763
azu-wakinoza <wakinoza@gmail.com>
2025-06-02 11:29:44 +0900

    toString()のオーバーライド
    「スッキリわかるJava入門 実践編」p24
    全てのクラスは継承元をたどると、Objectクラスに行きつく。Objectクラスは、全クラスに最低限掃除して欲しい機能を持っている。そのため、全クラスは最低限の機構を保証されている。toString()メソッドもその一つ。「インスタンスの内容を、人が読んで理解できる文字列に表現して返す」ことを責務として与えられている。しかし、ObjectクラスのtoSrting()メソッドは最低限の処理しか記載されていないため、クラスを作った際は、toString()をオーバーライドして、そのクラスに適切な処理に変更する必要がある。

---
+++ Study4Hero.java
@@ -0,0 +1,9 @@
+public class Study4Hero {
+  private String name;
+  private int hp, mp;
+  
+  public String toString(){
+      return "勇者（名前＝" + this.name + "/HP＝" + this.hp + "/MP＝" + this.mp + ")";
+  }
+}
+
