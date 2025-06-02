8ed150ee0b1376d86b09c621588963fcc8bad816
azu-wakinoza <wakinoza@gmail.com>
2025-06-02 11:59:54 +0900

    hashCode()のオーバーライド
    「スッキリわかるJava入門 実践編」p37
    ArrayListで要素の検索や削除を行う場合、内部ではequals()による等価判定を行なっている。そのため、「等価判定」を適切にオーバーライドしていないクラスのインスタンスをArrayListに格納すると、要素の削除や検索が思ったように動かないというエラーに繋がってしまう。
    HashSetを利用する場合は、equals()以外にhashCOde()を適切にオーバーライドする必要がある。
    hashCode()もObjectクラスで定義されているメソッドだ。「任意の個数の引数を受け取り、その引数に基づいたハッシュコードを生成する」という責務を担っている。
    ArrayListは要素の検索のためにequals()を使っていたが、HashSetはequals()以外にhashCOde()も利用している。
    HashSetでremove()メソッドを呼び出すと、JVMはまず、１つ１つの要素にhashCOde()でハッシュコードを確認し、ハッシュコードが一致するものにさらにequals()メソッドで等価判定を行なうという２段階の確認を行なっている。その理由は、equals()による等価判定が、比較的時間がかかるという欠点があるためだ。１つ１つequalsでチェックするより、高速に実施できるhashCode()でスクリーニングする方が、全体としての効率が良いためだ。
    hashCode()もObjectクラスの定義そのままでは使い物にならないことが多いため、新しいクラスを定義する際は、hashCode()メソッドもオーバーライドしてそのクラスに適切な処理に上書きする必要がある。

--- Study4Hero.java
+++ Study4Hero.java
@@ -1,9 +1,16 @@
+import java.util.Objects;
+
+
 public class Study4Hero {
   private String name;
   private int hp, mp;
   
   public String toString(){
       return "勇者（名前＝" + this.name + "/HP＝" + this.hp + "/MP＝" + this.mp + ")";
+  }
+
+  public int hashCode(){
+    return Objects.hash(this.name,this.hp);
   }
 }
 
