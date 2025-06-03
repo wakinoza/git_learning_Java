62b2ea06dddbb301d554ccf3daec5abaf6917c31
azu-wakinoza <wakinoza@gmail.com>
2025-06-03 14:14:47 +0900

    深いコピー
    「スッキリわかるJava入門 実践編」p44
    インスタンスをコピーする必要がある場合、新しい変数に代入するだけでは、参照がコピーされるだけで、同じインスタンスを指しています。コピー先でインスタンスが変更されれば、コピー元のインスタンスも変更されます。そのため、内容を変更されたくないインスタンスをコピーする場合は、インスタンス自身をコピーしなくてはならない。
    インスタンスを複製するには、①別のインスタンスをnewする②全てのフィールドを新しいインスタンスにコピーする、という手順が必要。コピーが必要な旅にこれらを記述するのは煩雑なので、Object
    クラスから継承しているclone()メソッドをオーバーライドして、定義する。clone()メソッドは「自分自身の複製インスタンスを作って返す」という責務を負っている。
    clone()メソッドをオーバーライドする際は、Cloneableインターフェース実装する。Cloneableインターフェースは、「このクラスはclone()を実装している」ことを表明するために存在しているため、インターフェースはメソッドをいつも持っていない。こういった特定の情報を表明するためだけに利用するインターフェースを「マーカーインターフェース」という。
    また、インスタンスのコピーにも「浅いコピー」と「深いコピー」の２種類が存在する。浅いコピーはそのインスタンスのみを複製するもの。自身のインスタンスAが別のインスタンスBの参照を持っている場合、インスタンスBは参照をコピーするだけにとどまる為、インスタンスBの内容が変更される可能性が残っている。一方、深いコピーは、そのインスタンスが参照しているインスタンスも含めて複製する。その為、インスタンスBも含めて、元のインスタンスに影響することはない。
    深いコピーを実装する場合は、インスタンスBのクラスにもCloneableインターフェースを実装し、clone()メソッドをオーバーライドする必要がある。

--- Hero.java
+++ Hero.java
@@ -1,6 +1,7 @@
-public class Hero {
+public class Hero implements Cloneable{
     private String name;
     private int hp;
+    Sword sword;
     
     public String getName(){
         return this.name;
@@ -13,6 +14,14 @@ public class Hero {
             this.hp = 0;
         }
         this.hp = newHp; 
+    }
+
+    public Hero clone(){
+        Hero result = new Hero();
+        result.name = this.name;
+        result.hp = this.hp;
+        result.sword = this.sword.clone();
+        return result;
     }
 
 }
