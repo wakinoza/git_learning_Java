096326f7d3dfd59b63afaff63967a15bc44160dc
azu-wakinoza <wakinoza@gmail.com>
2025-06-10 09:57:56 +0900

    ラムダ式の練習
    「スッキリわかるJava入門実践編」p123
    関数型インターフェースを利用すると、変数に関数の参照を代入することができた。それらの記述は、ラムダ式を利用するとさらに簡潔に記述できる。
    基本の構文は (引数) -> {処理}と記述する。
    ラムダ式は条件が合えば、さらに記述を省略できる。
    １、代入式の右辺でラムダ式を使うときは、引数の方を省略できる。JVMが左辺の関数型インターフェースの抽象メソッドの型を確認し、メソッドの引数の型を自動的に利用してくれるため。
    ２、引数が１つの場合は、()を省略できる。２つ以上やまたは引数なしの時は省略できない。
    ３、処理が1つの場合は、{}とreturnを省略できる。処理が２つ以上のときは{}とreturnを省略できる。処理が２つ以上のときはが必要
    
    ラムダ式は、さまざまなとことで用いることができる。
    例としては、Comparatorインターフェースがある。Collectionsクラスのsort()の第２引数で並び替え順を指示する場合は、ComparetorインターフェースのCompare()メソッドを実装するクラスを記述し、実装クラスのインスタンスをSort()メソッドに渡す必要があった。しかし、sort()メソッドの第２引数に直接ラムダ式を書くこともできる。

--- Main.java
+++ Main.java
@@ -1,18 +1,15 @@
+import java.util.function.*;
 import java.util.*;
 
 public class Main {
-    public static Optional<String> decorate(String s, char c){
-      String r;
-      if (s == null || s.length() == 0) {
-        r = null;
-      } else {
-        r = c + s + c;
-      }
-      return Optional.ofNullable(r);
-    }  
+  public static void main(String[] args) {
+    Function<String, Integer> func = 
+      (String s) -> { return s.length();}; // s -> s.length();とも省略できる
+    int n = func.apply("Java");
+    System.out.println("文字列「Java」は" + n + "文字です");
 
-    public static void main(String[] args) {
-      Optional<String> s = decorate("", '*');
-      System.out.println(s.orElse("nullのため処理できません"));
-    }
+    List<Account> list = new ArrayList<>();
+    Collections.sort(list,(x,y) -> (x.zandaka - y.zandaka));
+  }
 }
+
