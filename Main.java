4265ee739b4191d05990df7046eedd291b38d586
azu-wakinoza <wakinoza@gmail.com>
2025-06-11 22:07:51 +0900

    steamの例題
    「スッキリわかるJava入門実践編」p125
    高階関数を用いると、配列やリストなどの処理を直観的にわかりやすく喜寿するつことができる。これを「宣言的な記述」という
    宣言的な記述は、目的だけを表現し、その表現手段までは踏み込まないのが特徴である。Javaでは、多くの高階関数がストリームAPIとして提供されている。
    ストリームには、データ処理メソッドや中間処理メソッドがある。# Please enter the commit message for your changes. Lines starting

--- Main.java
+++ Main.java
@@ -3,13 +3,8 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    Function<String, Integer> func = 
-      (String s) -> { return s.length();}; // s -> s.length();とも省略できる
-    int n = func.apply("Java");
-    System.out.println("文字列「Java」は" + n + "文字です");
-
-    List<Account> list = new ArrayList<>();
-    Collections.sort(list,(x,y) -> (x.zandaka - y.zandaka));
+    List<Hero> heros = new ArrayList<>();
+    boolean anyoneKnockOut = heros.stream().anyMatch(h -> h.hp == 0);
   }
 }
 
