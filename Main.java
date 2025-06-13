66affbf38103173c1d8f03421204e77f42e91b6e
azu-wakinoza <wakinoza@gmail.com>
2025-06-13 09:51:06 +0900

    タイムゾーン
    「スッキリわかるJava入門 実践編」p149
    ロケールでは時間と言語の情報を管理していました。
    JVMは時間帯についても管理しています。これをタイムゾーンといい、プログラムが動作しているPCの置かれた地域で使われている標準時と世界標準時との時差で表されます。
    タイムゾーンは、java.util.TimeZoneクラスを用いて操作します。

--- Main.java
+++ Main.java
@@ -1,16 +1,17 @@
 import java.util.*;
-import java.text.*;
 
 public class Main {
   public static void main(String[] args) {
-    Locale loc = Locale.getDefault(); //デフォルトロケールを取得
-    System.out.println(loc.getCountry() + "-" + loc.getLanguage());
-    String now = (new SimpleDateFormat()).format(new Date());
-    if (loc.getLanguage().equals("ja")){
-      System.out.println("現在の時刻は" + now);
+    TimeZone tz = TimeZone.getDefault();
+    System.out.print("現在のタイムゾーン：");
+    System.out.println(tz.getDisplayName());
+    if (tz.useDaylightTime()) {
+      System.out.println("夏時間を採用しています");
     } else {
-      System.out.println("Current time is" + now);
+      System.out.println("夏時間を採用していません");
     }
+    System.out.print("世界標準時との時差は");
+    System.out.println(tz.getRawOffset() / 360000 + "時間"); //ミリ秒を時間に変換
   }
 }
 
