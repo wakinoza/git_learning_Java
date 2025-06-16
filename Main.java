40d75499ba7aa8f403fdde21c64b1b3b95794023
azu-wakinoza <wakinoza@gmail.com>
2025-06-16 11:18:21 +0900

    ファイルの書き込み
    「スッキリわかる Java入門実践編」p195
    標準APIwo使えば、必要なファイルを読み取ったり書き込んだりできるようになる。
    ファイルの読み書きは、必ず３つの手段が必要になる。
    １、ファイルを開く
    ２、ファイルのデータを読み込んだり、書き込んだりする
    ３、ファイルを閉じる。
    ファイルを読み書きする方法には、ランダムアクセスとシーケンシャルアクセスの２種類がある。
    ランダムアクセスは、ファイルのあちこちを自由に読み書きできるが、読み書きの速度が遅いため、大きなファイルの扱いだと処理が遅くなる。
    シーケンシャルアクセスは、読み書き速度が速いため、大きなファイルも扱いやすいが、先頭から順番にした読み書きができない。
    FileWriterクラスは、シーケンシャルアクセスの１つ。コンすたらクタの第1引数にファイル名を指定する。指定したファイルがあればそれを開き、指定したファイルがなければ自動的に青の名前のファイルを作成して開く。
    第2引数にtrueを入れると、データの末尾から追記していく。第2引数が省略されるかfalseの場合は、ファイルの先頭からデータを上書きしていく。

--- Main.java
+++ Main.java
@@ -1,17 +1,11 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
-  public static void main(String[] args) {
-    TimeZone tz = TimeZone.getDefault();
-    System.out.print("現在のタイムゾーン：");
-    System.out.println(tz.getDisplayName());
-    if (tz.useDaylightTime()) {
-      System.out.println("夏時間を採用しています");
-    } else {
-      System.out.println("夏時間を採用していません");
-    }
-    System.out.print("世界標準時との時差は");
-    System.out.println(tz.getRawOffset() / 360000 + "時間"); //ミリ秒を時間に変換
+  public static void main(String[] args)throws IOException {
+    FileWriter fw = new FileWriter("c:¥¥rpgsave.dat",true);
+    fw.write('A');
+    fw.flush();
+    fw.close();
   }
 }
 
