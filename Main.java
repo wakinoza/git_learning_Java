51ea4d227dada0f5982b6670bfc3e20e25b82290
azu-wakinoza <wakinoza@gmail.com>
2025-06-16 11:47:46 +0900

    バイナリーファイルの読み書き
    「スッキリわかるJava入門 実践編」p201
    ファイルには、文字として人間に解釈可能なデータが入るテキストファイルと、文字としては解釈できないデータが入るバイナリファイルに分類できる。
    バイナリファイルにバイト列を書き込むためにはFileOutputStreamクラス、予期取るためにはFileInputStreamクラスを利用する

--- Main.java
+++ Main.java
@@ -2,19 +2,10 @@ import java.io.*;
 
 public class Main {
   public static void main(String[] args)throws IOException {
-    FileReader fw = new FileReader("rpgsave.dat");
-    System.out.println("全てのデータを読み込んで表示します");
-    int i = fw.read();
-    //FileReaderは基本的に１文字ずつ読み取る
-     //読み込んだ文字をint型で受け取る理由は、ファイルを最後まで読み終わったらread()が戻り値として「−１」が返すため。Char型では「−１」では扱えないため、int型で戻り値を受け取っている。    
-    while (i != -1){
-      char c = (char)i;
-      //read()の戻り値が「−１」でない場合は、char型にキャストする。
-      System.out.println(c);
-      i = fw.read();
-    }
-    System.out.println("ファイルの末尾に到着しました");
-    fw.close();
+    FileOutputStream fos = new FileOutputStream(("rpgsave.dat",true));
+    fos.write(65);
+    fos.flush();
+    fos.close();
   }
 }
 
