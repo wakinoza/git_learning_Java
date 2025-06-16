1309415df616fd753dece13ef855f8a0f172175b
azu-wakinoza <wakinoza@gmail.com>
2025-06-16 11:37:33 +0900

    ファイルの読み取り
    「スッキリわかる Java入門 実戦編」p199
    ファイル情報を読み取る場合は、FileReaderクラスを用いる。FileWriterクラスは文字列を扱えたが、FileReaderは基本的に１文字ずつか読み取れない。その上、読み終わった場合はread()は戻り値として「−１」を返すため、char型では扱えない。
    そのため、read()の戻り値はint型で受け取る。受け取ったのち、「−１」でない場合は、char型にキャストする。

--- Main.java
+++ Main.java
@@ -2,9 +2,18 @@ import java.io.*;
 
 public class Main {
   public static void main(String[] args)throws IOException {
-    FileWriter fw = new FileWriter("c:¥¥rpgsave.dat",true);
-    fw.write('A');
-    fw.flush();
+    FileReader fw = new FileReader("rpgsave.dat");
+    System.out.println("全てのデータを読み込んで表示します");
+    int i = fw.read();
+    //FileReaderは基本的に１文字ずつ読み取る
+     //読み込んだ文字をint型で受け取る理由は、ファイルを最後まで読み終わったらread()が戻り値として「−１」が返すため。Char型では「−１」では扱えないため、int型で戻り値を受け取っている。    
+    while (i != -1){
+      char c = (char)i;
+      //read()の戻り値が「−１」でない場合は、char型にキャストする。
+      System.out.println(c);
+      i = fw.read();
+    }
+    System.out.println("ファイルの末尾に到着しました");
     fw.close();
   }
 }
