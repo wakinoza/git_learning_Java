834d79acdfc2c74c980a74cdfca8d8f9e328ca58
azu-wakinoza <wakinoza@gmail.com>
2025-06-17 12:09:26 +0900

    プロパティ形式の書き込み
    「スッキリわかるJava入門 実践編」p236
    Javaのプログラムからデータをプロパティファイル形式で保存するためには、propertiesインスタンスにsetProperty()でキーと値のペアをセットした上で、store()を呼び出す。
    プロパティファイル形式の書き込みでは、ペアが書きもまれる順番は、プログラムに書いた順番になるとは限らない点が注意。

--- Main.java
+++ Main.java
@@ -3,15 +3,13 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args)throws Exception{
-    Reader fr = new FileReader("C:¥¥rpgdata.properties");
+    Writer fw = new FileWriter("c:¥¥rpgsave.properties");
     Properties p = new Properties();
-    p.load(fr);  //ファイルの内容を読み取る
-    String name = p.getProperty("heroName");
-    String strHp = p.getProperty("heroHP");
-    int hp = Integer.parseInt(strHp);
-    System.out.println("勇者の名前：" + name);
-    System.out.println("勇者のHP：" + hp);
-    fr.close();
+    p.setProperty("heroName", "アサカ");
+    p.setProperty("heroHp", "62");
+    p.setProperty("heroMp", "45");
+    p.store(fw,"勇者の情報"); //ファイルへ書き出す
+    fw.close();
   }
 }
 
