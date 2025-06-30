f900ae96f024faa7ae63435ac7f7ff8929e3e830
azu-wakinoza <wakinoza@gmail.com>
2025-07-01 08:27:14 +0900

    Threadの利用
    JVMは、基本的に単一スレッドの逐次処理を行う。つまり、ある命令を完全に実行してから、次の命令を実行するという仕組みだ。しかし、時に、複数の命令を同時に実行して欲しい場合がある、そのような時に利用するのがThreadというAPI。
    Threadを利用することで、スレッドを生成し、複数の命令を同時に実行できるようになる。
    Threadの使い方の基本は、
    １、Threadクラスを継承して、run()をオーバーライドする。この時、run()に別のスレッドで処理したい内容を書き込む。
    ２、別スレッドの実行を開始したい場所で、1のクラスのインスタンスを生成し、start()を呼び出す。

--- Main.java
+++ Main.java
@@ -1,21 +1,14 @@
-import java.io.*;
-import java.net.*;
+import java.util.*;
 
 public class Main {
   public static void main(String[] args)throws Exception{
-    Socket sock = new Socket("dokojava.jp",80);
-    InputStream is = sock.getInputStream();
-    OutputStream os = sock.getOutputStream();
-    os.write("GET /index.html HTTP/1.0¥r¥n".getBytes());
-    os.write("¥r¥n".getBytes());
-    os.flush();
-    InputStreamReader isr = new InputStreamReader(is);
-    int i = isr.read();
-    while (i != -1){
-      System.out.print((char) i);
-      i = isr.read();
-    }
-    sock.close();
+    System.out.println("止めるには「STOP」を入力してください");
+    System.out.println("カウントダウンを始めます");
+    Thread t = new PrintingThread();
+    t.start();
+    String input = new Scanner(System.in).nextLine();
+    System.out.println("入力文字列：" + input);
+    System.out.println("停止処理は未完成です");
   }
 }
 
