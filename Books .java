228923cc1093ddef78e26804957e59cd8044a827
azu-wakinoza <wakinoza@gmail.com>
2025-07-02 12:09:15 +0900

    スッキリわかるjava入門 実践編 練習問題１−３

--- Books .java
+++ Books .java
@@ -1,12 +1,24 @@
 import java.util.*;
 
-public class Books implements Comparable ,Cloneable<Books>{
+public class Books implements Comparable, Cloneable<Books>{
   private String title;
   private Date publishDate;
   private String comment;
 
+  public Books (String title, Date publishDate, String comment){
+    this.title = title;
+    this.publishDate = publishDate;
+    this.comment = comment;
+  }
+
   public String getTitle(){
     return this.title;
+  }
+  public Date getPublishDate(){
+    return this.publishDate;
+  }
+  public String getComment(){
+    return this.comment;
   }
   public boolean equals(Object o){
     if (o == this) return true;
--- Main.java
+++ Main.java
@@ -1,8 +1,19 @@
 import java.util.*;
+import java.text.SimpleDateFormat;
 
 public class Main {
   public static void main(String[] args)throws Exception{
-    new Thread(new Battle()).start();
+    SimpleDateFormat f = new SimpleDateFormat("yyyy/mm//dd");
+    ArrayList<Books> books = new ArrayList<>();
+    books.add(new Books("Java入門", f.parse("2011/10/07"), "スッキリわかる"));
+    books.add(new Books("Python入門", f.parse("2019/06/11"), "カレーが食べたくなる"));
+    books.add(new Books("C言語入門", f.parse("2018/06/21"), "ポインタも自由自在"));
+
+    Collections.sort(books,new TitleComparator());
+
+    for (Books book : books){
+      System.out.println(book.getTitle() + f.format(book.getPublishDate()) + book.getComment());
+    }
   }
 }
 
