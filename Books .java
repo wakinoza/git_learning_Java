ba044c4d459fcfbe4d99347dbac664c07cd4f0ce
azu-wakinoza <wakinoza@gmail.com>
2025-07-02 11:36:06 +0900

    スッキリわかるJava入門 実践編 練習問題１−１

---
+++ Books .java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Books implements Comparable ,Cloneable<Books>{
+  private String title;
+  private Date publishDate;
+  private String comment;
+
+  public boolean equals(Object o){
+    if (o == this) return true;
+    if (o == null) return false;
+    if (!(o instanceof Books)) return false;
+    Books r = (Books)0;
+    if (!(this.title.equals (r.title) && this.publishDate.equals(r.publishDate))) {
+      return false;
+    }
+    return true;
+  }
+
+  public int hashCode(){
+    return Objects.hash(this.title, this.publishDate);
+  }
+
+  public int compareTo(Books obj){
+    if (this.publishDate < obj.publishDate) {
+      return  -1;
+    }
+    if (this.publishDate > obj.publishDate) {
+      return 1;
+    }
+    return 0;
+  }
+
+  public Books clone(){
+    Books result = new Books();
+    result.title = this.title;
+    result.publishDate = (Date)this.publishDate.clone();
+    result.comment = this.comment;
+  }
+
+
+}
