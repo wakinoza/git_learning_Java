ad06cf959a16f1d75c57e21b72a82e39016a15d7
azu-wakinoza <wakinoza@gmail.com>
2025-07-07 10:49:07 +0900

    スッキリわかるjava入門 実践編 練習問題7-1

--- Main.java
+++ Main.java
@@ -1,30 +1,13 @@
 import java.io.*;
-import java.util.zip.GZIPOutputStream;
+import java.util.*
 
 public class Main {
-  public static void main(String[] args){
-    if (args.length != 2){
-      System.out.println("起動パラメータの指定が不正です");
-      return;
-    }
-    String inFile = args[0];
-    String outFile = args[1];
-
-    try (
-      FileInputStream fis = new FileInputStream(inFile);
-      FileOutputStream fos = new FileOutputStream(outFile);
-      BufferedOutputStream bos = new BufferedOutputStream(fos);
-      GZIPOutputStream gzos = new GZIPOutputStream(bos);
-    ){
-      int i = fis.read();
-      while (i != -1){
-        fos.write(i);
-        i = fis.read();
-      }
-      gzos.flush();
-    } catch (IOException e){
-      System.out.println("ファイル処理に失敗しました");
-    }
+  public static void main(String[] args)throws Exception{
+    Reader fr = new FileReader("pref.properties");
+    Properties p = new Properties();
+    p.load(fr);
+    System.out.println(p.getProperty("aichi.capital") + ":" + p.getProperty("aichi.food"));
+    fr.close();
   }
 }
 
