f0627306463023e890124a4b2a02b242a3e62096
azu-wakinoza <wakinoza@gmail.com>
2025-07-07 10:52:32 +0900

    スッキリわかるjava入門 実践編 練習問題7-2

--- Main.java
+++ Main.java
@@ -1,13 +1,10 @@
 import java.io.*;
-import java.util.*
+import java.util.*;
 
 public class Main {
   public static void main(String[] args)throws Exception{
-    Reader fr = new FileReader("pref.properties");
-    Properties p = new Properties();
-    p.load(fr);
-    System.out.println(p.getProperty("aichi.capital") + ":" + p.getProperty("aichi.food"));
-    fr.close();
+    ResourceBundle rb = ResourceBundle.getBundle("pref");
+    System.out.println(rb.getString("aichi.capital") + ":" + rb.getString("aichi.food"));
   }
 }
 
