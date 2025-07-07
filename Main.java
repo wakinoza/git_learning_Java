19ffd2e67811f936be0b06909ef57241b338e037
azu-wakinoza <wakinoza@gmail.com>
2025-07-07 10:22:34 +0900

    スッキリわかるJava入門 実践編 練習問題6-2

--- Main.java
+++ Main.java
@@ -1,19 +1,30 @@
 import java.io.*;
+import java.util.zip.GZIPOutputStream;
 
 public class Main {
-  public static void main(String[] args)throws Exception{
+  public static void main(String[] args){
+    if (args.length != 2){
+      System.out.println("起動パラメータの指定が不正です");
+      return;
+    }
     String inFile = args[0];
     String outFile = args[1];
-    FileInputStream fis = new FileInputStream(inFile);
-    FileOutputStream fos = new FileOutputStream(outFile);
-    int i = fis.read();
-    while (i != -1){
-      fos.write(i);
-      i = fis.read();
+
+    try (
+      FileInputStream fis = new FileInputStream(inFile);
+      FileOutputStream fos = new FileOutputStream(outFile);
+      BufferedOutputStream bos = new BufferedOutputStream(fos);
+      GZIPOutputStream gzos = new GZIPOutputStream(bos);
+    ){
+      int i = fis.read();
+      while (i != -1){
+        fos.write(i);
+        i = fis.read();
+      }
+      gzos.flush();
+    } catch (IOException e){
+      System.out.println("ファイル処理に失敗しました");
     }
-    fos.flush();
-    fos.close();
-    fis.close();
   }
 }
 
